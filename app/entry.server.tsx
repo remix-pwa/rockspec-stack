c5b1c3fea51bb09c7b5a4363661669f07ff1c84d
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-17 20:58:19 +0100

    Reading env vars

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,6 +1,7 @@
 import type { EntryContext } from "@remix-run/node";
 import { RemixServer } from "@remix-run/react";
 import { renderToString } from "react-dom/server";
+import "dotenv/config"
 
 export default function handleRequest(
   request: Request,
