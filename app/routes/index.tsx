4cebc386d355f516c4a86fd122a481bc52cbdc9b
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-30 21:16:38 +0100

    fix:Removed stray bracket in `index`

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -73,7 +73,6 @@ export default function Index() {
                   </Link>
                 </div>
               )}
-              )
             </div>
             <a href="https://remix.run" className="mx-auto w-max">
               <img
